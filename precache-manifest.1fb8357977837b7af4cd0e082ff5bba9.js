c0c2d33474de53e13175590f9a8057f3f942c53d
Julia <julesss@posteo.de>
2022-09-26 19:13:17 +0200

    Updates

---
+++ precache-manifest.1fb8357977837b7af4cd0e082ff5bba9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7758116005f820394c9c",
+    "url": "/React-Movie-Finder/static/css/main.10bfbb4b.chunk.css"
+  },
+  {
+    "revision": "7758116005f820394c9c",
+    "url": "/React-Movie-Finder/static/js/main.77581160.chunk.js"
+  },
+  {
+    "revision": "4bb05d8fed1e4d6e8559",
+    "url": "/React-Movie-Finder/static/js/1.4bb05d8f.chunk.js"
+  },
+  {
+    "revision": "d2ae237ad20686471a2a",
+    "url": "/React-Movie-Finder/static/css/2.ebce27ee.chunk.css"
+  },
+  {
+    "revision": "d2ae237ad20686471a2a",
+    "url": "/React-Movie-Finder/static/js/2.d2ae237a.chunk.js"
+  },
+  {
+    "revision": "e31cc3164a9c4e1ac523",
+    "url": "/React-Movie-Finder/static/js/runtime~main.e31cc316.js"
+  },
+  {
+    "revision": "c5c3fce9fddfe92b2722064669bd1b81",
+    "url": "/React-Movie-Finder/index.html"
+  }
+];
